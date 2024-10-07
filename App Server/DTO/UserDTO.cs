6966eb542b017ed06713479fbecd1d2d5926c95f
User <User@LAB2-8>
2024-10-07 15:24:38 +0300

    DTO Constructor

--- App Server/DTO/UserDTO.cs
+++ App Server/DTO/UserDTO.cs
@@ -2,10 +2,21 @@
 {
     public class UserDTO
     {
-        public string Username { get; set; }
+        public string Username { get; set; } = null!;
         public string? Email { get; set; }
         public string? UserPassword { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+
+        public UserDTO() { }
+        public UserDTO(Models.User useri)
+        {
+            this.Username = useri.Username;
+            this.Email = useri.Email;
+            this.FirstName = useri.FirstName;
+            this.LastName = useri.LastName;
+            this.UserPassword = useri.UserPassword;
+
+        }
     }
 }
