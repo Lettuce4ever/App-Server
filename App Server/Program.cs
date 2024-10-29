f6671d2ab4ef950072b51e40194e4c600c156771
User <User@LAB2-8>
2024-10-29 12:34:16 +0200

    Swagger not working

--- App Server/Program.cs
+++ App Server/Program.cs
@@ -1,6 +1,8 @@
 using App_Server.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.AspNetCore.Builder;
 
 namespace App_Server
 {
@@ -14,59 +16,42 @@ namespace App_Server
 
             builder.Services.AddControllers();
 
+            string connectionString = builder.Configuration.GetConnectionString("GameDbContext");
+            builder.Services.AddDbContext<GameDbContext>(options => options.UseSqlServer(connectionString));
 
-            //Add Database to dependency injection
-            builder.Services.AddDbContext<GameDbContext>(
-                    options => options.UseSqlServer("Server = (localdb)\\MSSQLLocalDB;Initial Catalog=Game_DB;User ID=TaskAdminLogin;Password=DBGalPass; Trusted_Connection=True; MultipleActiveResultSets = true"));
-           
-            
-            #region Add Session
             builder.Services.AddDistributedMemoryCache();
             builder.Services.AddSession(options =>
             {
                 options.IdleTimeout = TimeSpan.FromMinutes(20);
-                options.Cookie.HttpOnly = false;
+                options.Cookie.HttpOnly = true;
                 options.Cookie.IsEssential = true;
             });
-            #endregion
-            
-            #region for debugginh UI
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-/*            builder.Services.AddSwaggerGen();*/
-            #endregion
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            #region for debugginh UI
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                //app.UseSwagger();
-                //app.UseSwaggerUI();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
-            #endregion
-
-
-            #region Add Session
-            app.UseSession(); //In order to enable session management
-            #endregion 
-
-            // Configure the HTTP request pipeline.
-
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            app.UseSession();
 
+            app.UseAuthorization();
 
             app.MapControllers();
 
             app.Run();
+
+           
         }
 
-        private static void connectionString(SqlServerDbContextOptionsBuilder builder)
-        {
-            throw new NotImplementedException();
-        }
+
     }
 }
